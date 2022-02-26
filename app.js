02cf9aaef721d858efd22330b0b82fe3d0c9b6c1
Almir Ramos <almirrdsnegro@gmail.com>
2022-02-26 05:13:22 -0300

    atualizando site

---
+++ app.js
@@ -0,0 +1,7 @@
+
+let btnClicado = document.querySelector('.btn')
+let imgAposOClick = document.querySelector('.imgClicada')
+
+btnClicado = addEventListener('click', () =>{
+    imgAposOClick.atyle.width = '400px'    
+});
